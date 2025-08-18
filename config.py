ea005d0445cf5a3ffc47c229041b2238f1ea3332
harsh969339 <harshkumar773951@gmail.com>
2025-08-18 14:45:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/harsh969339/HARSH_MUSIC",
+    "https://github.com/harsh969339/HarshMusic",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
